4d46f2d2c789aa2f398493d0e57fff5718ae2cfd
Hamzaalsafi <hamzasf345@gmail.com>
2024-11-14 21:13:26 +0300

    add the sec project

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,12 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
 
-export function Gallery() {
-  const images = [
-    "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-    "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-  ];
+export function Gallery({images}) {
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const intervalRef = useRef(null);
@@ -55,51 +50,60 @@ export function Gallery() {
 
   const handleBubbleClick = (index) => {
     setCurrentIndex(index);
-    startAutoplay(); // Restart autoplay after manual interaction
+    startAutoplay(); 
   };
 
-  // Function to handle next/prev buttons
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    startAutoplay(); // Restart autoplay after manual interaction
-  };
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-    startAutoplay(); // Restart autoplay after manual interaction
-  };
+  
+ 
 
   return (
     <motion.div
-    initial={{ x: '100%', opacity: 0, scale: 0.9 }}
-    animate={{ x: 0, opacity: 1, scale: 1 }}
-    exit={{ x: '-100%', opacity: 0, scale: 1.1 }}
+    initial={{  opacity: 0, scale: 0.9 }}
+    animate={{ opacity: 1, scale: 1 }}
+    exit={{ opacity: 0, scale: 1.1 }}
     transition={{
       type: 'spring',
       stiffness: 300,
       damping: 25,
-      duration: 1.2,
-      ease: [0.68, -0.55, 0.27, 1.55], // Smooth easing
+      duration: 1,
+      ease: [0.68, -0.55, 0.27, 1.55],
     }}
-    ref={scrollRef} className="relative rounded-xl overflow-hidden w-full h-full">
-      {/* Image display with Framer Motion for transition */}
+    ref={scrollRef} className="relative rounded-xl flex items-center justify-center w-full h-full">
+ 
       <motion.img
+      
+
         src={images[currentIndex]}
         alt={`Image ${currentIndex + 1}`}
-        className="h-full w-full object-cover object-center"
+        className="h-auto max-h-[310px] w-auto " 
+         loading="lazy"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 1 }} // Fade in transition
+        transition={{ duration: 1 }} 
       />
 
-      {/* Bubble navigation */}
+  
       <div className="absolute bottom-4 left-1/2  transform -translate-x-1/2 flex space-x-2">
         {images.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => handleBubbleClick(index)}
-            className={` h-3  duration-350 shadow-xl rounded-full ${currentIndex === index ? 'w-9 ' : 'w-3'} bg-gray-300 transition-all`}
-          ></button>
+          <motion.div
+          animate={{
+            width: currentIndex === index ? '25px' : '12px',
+            backgroundColor: currentIndex === index ? 'rgba(239, 68, 68, 1)' : 'rgba(209, 213, 219, 1)', // Using rgba here
+          }}
+          transition={{
+            type: 'tween',
+            ease: "easeOut",
+            duration: 0.1, 
+          }}
+          whileHover={{
+            scale: currentIndex === index ? 1.3 : 1.1,
+            width: currentIndex === index ? '20px' : '17px',
+            boxShadow: '0px 0px 10px rgba(239, 68, 68, 0.5)', 
+          }}
+          key={index}
+          onClick={() => handleBubbleClick(index)}
+          className={` h-2.5 cursor-pointer  duration-350 shadow-xl p-1 border-2 border-opacity-25 border-red-700 rounded-full ${currentIndex === index ? 'w-7 ' : 'w-3'} bg-gray-300 transition-all`}
+        ></motion.div>
         ))}
       </div>
 
--- src/Project.js
+++ src/Project.js
@@ -3,14 +3,28 @@ import { motion } from "framer-motion";
 import  {useModeContext}  from './DarkModeProvider';
 import Tooltip from '@mui/material/Tooltip';
 import {Gallery} from './Gallery'
-export function Project() {
- const [activeTooltip, setActiveTooltip] = useState(null); // Track active tooltip
 
+
+export function Project() {
+  let kittyTaskPhone=[
+    "/kittyTaskPhone2.png",
+    "/kittyTaskPhone1.png",
+    "/kittyTaskPhone3.png"
+  ]
+  let kittyTaskMac=[
+    "/kittyTask1.png",
+    "/kittyTask2.png",
+    "/kittyTask3.png",
+    "/kittyTask4.png",
+    "/kittyTask5.png"
+  ]
+ const [activeTooltip, setActiveTooltip] = useState(null); 
+ const [showOptions,setShowOptions] = useState('laptop');
   const handleTooltipClick = (tooltipName) => {
     if (activeTooltip === tooltipName) {
-      setActiveTooltip(null); // Close the tooltip if it’s already open
+      setActiveTooltip(null);
     } else {
-      setActiveTooltip(tooltipName); // Open the tooltip
+      setActiveTooltip(tooltipName); 
     }
   };
 
@@ -26,9 +40,9 @@ export function Project() {
         </span>
       ))}
     </div>
-         <div className={`${mode!=="dark"?"text-zinc-900":"text-zinc-200"} h-full w-full pt-5   items-center flex flex-col`}>
-            <div className='flex justify-between h-fit rounded-xl p-4 pb-3 donation w-full '>
-               <div className='w-1/2 justify-start'>
+         <div className={`${mode!=="dark"?"text-zinc-900":"text-zinc-200"} h-full w-full pt-5 gap-5 md:gap-8  items-center flex flex-col`}>
+            <div className='flex md:flex-row flex-col phone  justify-between h-fit  rounded-xl p-4 pb-3 donation w-full '>
+               <div className='md:w-1/2  justify-start'>
                  <h1 className='text-2xl font-bold'>KittyTask</h1>
                  <h2 className='text-lg pt-1 font-bold'>Streamlined Task Management, Inspired by Trello</h2>
                  <p className={`text-[1.17rem] ${mode!=="dark"?"text-zinc-800":"text-zinc-300"}`}>  KittyTask brings task management to a new level with real-time collaboration, fully customizable boards, and smooth, intuitive animations. Experience powerful drag-and-drop functionality in a user-friendly interface.</p>
@@ -250,9 +264,323 @@ export function Project() {
                  </div>
                </div>
 
-               <div className='w-1/2'>
-              <Gallery/>
- 
+               <div className='md:w-1/2 md:mb-0 mb-5  relative'>
+               <div className='md:absolute relative z-50  text-slate-950 text-3xl gap-5 text-center mb-2 w-full flex justify-center items-center' >
+               <motion.img
+  onClick={() => setShowOptions("iphone")}
+  src="/iphone.svg"
+  alt="Iphone"
+  className={`w-8 h-8 md:h-10 md:w-10 px-1 cursor-pointer rounded-xl ${mode === "dark" && showOptions === "iphone" ? "bg-gray-300" : "bg-gray-300"}`}
+  animate={{
+    scale: showOptions === "iphone" ? 1.15 : 1, // Enlarges when selected
+    boxShadow: showOptions === "iphone" ? "0px 4px 15px rgba(0, 0, 0, 0.2)" : "none",
+    backgroundColor: showOptions === "iphone" ? "rgba(255, 255, 255, 1)" : "rgba(0, 0, 0, 0)", // Transparent black
+  }}
+  transition={{
+    duration: 0.2,
+    ease: "easeOut",
+  }}
+/>
+
+<motion.img
+  onClick={() => setShowOptions("laptop")}
+  src="/laptop2.svg"
+  alt="Laptop"
+  className={`w-8 h-8 md:h-10 md:w-10 p-1 cursor-pointer rounded-xl ${mode === "dark" && showOptions === "laptop" ? "bg-gray-300" : "bg-gray-300"}`}
+  animate={{
+    scale: showOptions === "laptop" ? 1.15 : 1,
+    boxShadow: showOptions === "laptop" ? "0px 4px 15px rgba(0, 0, 0, 0.2)" : "none",
+    backgroundColor: showOptions === "laptop" ? "rgba(255, 255, 255, 1)" : "rgba(0, 0, 0, 0)", // Transparent black
+  }}
+  transition={{
+    duration: 0.2,
+    ease: "easeOut",
+  }}
+/>
+
+               </div>
+               {showOptions === "laptop" && (
+   
+      <Gallery  images={kittyTaskMac} />
+    
+  )}
+  
+  {showOptions === "iphone" && (
+   
+      <Gallery images={kittyTaskPhone}  />
+   
+  )}
+               </div>
+            </div>
+            <div className='flex md:flex-row flex-col phone  justify-between h-fit  rounded-xl p-4 pb-3 donation w-full '>
+               <div className='md:w-1/2  justify-start'>
+                 <h1 className='text-2xl font-bold'>Interactive Drawing Tool</h1>
+                 <h2 className='text-lg pt-1 font-bold'>Intuitive Digital Art Creation, Built for All Devices</h2>
+                 <p className={`text-[1.17rem] ${mode!=="dark"?"text-zinc-800":"text-zinc-300"}`}>  Interactive Drawing Tool takes the drawing experience to a new level with a feature-rich interface designed for artists and creatives. Enjoy smooth animations, realistic tools, and full customization options, all crafted to deliver a seamless experience.</p>
+                 <h2 className='text-lg py-1 font-bold'>Key Features:</h2>
+                 <ul className={`list-disc text-[1.1rem] pl-5 ${mode!=="dark"?"text-zinc-800":"text-zinc-300"}`}>
+                   <li>Realistic Tools</li>
+                   <li>Shape Drawer</li>
+                   <li>Dynamic Text:Add, move, with drag-and-drop </li>
+                   <li>Undo/Redo</li>
+                   <li>Save & Delete</li>
+                   <li>Smooth Animations & User-Friendly Interface</li>
+                 </ul>
+                 <div className='flex gap-2.5  pt-2 items-center flex-wrap'>
+                    <Tooltip
+                      PopperProps={{
+                        modifiers: [
+                          {
+                            name: "offset",
+                            options: {
+                              offset: [0, -4]
+                            },
+                          },
+                        ],
+                      }}
+                      title="React"
+                      arrow
+                      open={activeTooltip === 'react'}
+                      onClose={() => setActiveTooltip(null)}
+                      onOpen={() => setActiveTooltip('react')}
+                      classes={{
+                        tooltip: mode === "dark" ? "custom-tooltip" : "custom-tooltip2",
+                        arrow:mode === "dark" ? "custom-arrow":"custom-arrow2",
+                      }}
+                    >
+                      <motion.div
+                       whileHover={{
+                        rotate: 180,  
+                        scale: 1.15,    
+                        transition: {
+                          type: "spring",
+                          stiffness: 300,
+                          damping: 20,  
+                        },
+                      }}
+                      initial={{ opacity: 0 }} 
+                      animate={{ opacity: 1 }}  
+                      exit={{ opacity: 0 }}    
+                      
+                        onClick={() => handleTooltipClick('react')}
+                        className="w-10 h-10 cursor-pointer donation rounded-full flex items-center justify-center"
+                      >
+                        <img className="h-8 w-8" src="/react.svg" alt="React Logo" />
+                      </motion.div>
+                    </Tooltip>
+
+                    <Tooltip
+                     PopperProps={{
+                      modifiers: [
+                        {
+                          name: "offset",
+                          options: {
+                            offset: [0, -4]
+                          },
+                        },
+                      ],
+                    }}
+                    classes={{
+                      tooltip: mode === "dark" ? "custom-tooltip" : "custom-tooltip2",
+                      arrow:mode === "dark" ? "custom-arrow":"custom-arrow2",
+                    }}
+                      title="Tailwind"
+                      arrow
+                      open={activeTooltip === 'tailwind'}
+                      onClose={() => setActiveTooltip(null)}
+                      onOpen={() => setActiveTooltip('tailwind')}
+                    >
+                        <motion.div
+                       whileHover={{
+                        rotate: 180,  
+                        scale: 1.15,    
+                        transition: {
+                          type: "spring",
+                          stiffness: 300,
+                          damping: 20,  
+                        },
+                      }}
+                      initial={{ opacity: 0 }} 
+                      animate={{ opacity: 1 }}  
+                      exit={{ opacity: 0 }}    
+                        onClick={() => handleTooltipClick('tailwind')}
+                        className="w-10 h-10 cursor-pointer donation rounded-full flex items-center justify-center"
+                      >
+                        <img className="h-9 w-9" src="/tailwind.png" alt="Tailwind Logo" />
+                      </motion.div>
+                    </Tooltip>
+
+                    <Tooltip
+                     PopperProps={{
+                      modifiers: [
+                        {
+                          name: "offset",
+                          options: {
+                            offset: [0, -4]
+                          },
+                        },
+                      ],
+                    }}
+                    classes={{
+                      tooltip: mode === "dark" ? "custom-tooltip" : "custom-tooltip2",
+                      arrow:mode === "dark" ? "custom-arrow":"custom-arrow2",
+                    }}
+                      title="CSS"
+                      arrow
+                      open={activeTooltip === 'css'}
+                      onClose={() => setActiveTooltip(null)}
+                      onOpen={() => setActiveTooltip('css')}
+                    >
+                      <motion.div
+                      whileHover={{
+                        rotate: 180,  
+                        scale: 1.15,    
+                        transition: {
+                          type: "spring",
+                          stiffness: 300,
+                          damping: 20,  
+                        },
+                      }}
+                      initial={{ opacity: 0 }} 
+                      animate={{ opacity: 1 }}  
+                      exit={{ opacity: 0 }}   
+                        onClick={() => handleTooltipClick('css')}
+                        className="w-10 h-10 cursor-pointer donation rounded-full flex items-center justify-center"
+                      >
+                        <img className="h-7 w-7" src="/css.svg" alt="CSS Logo" />
+                      </motion.div>
+                    </Tooltip>
+
+                    <Tooltip
+                     PopperProps={{
+                      modifiers: [
+                        {
+                          name: "offset",
+                          options: {
+                            offset: [0, -4]
+                          },
+                        },
+                      ],
+                    }}
+                    classes={{
+                      tooltip: mode === "dark" ? "custom-tooltip" : "custom-tooltip2",
+                      arrow:mode === "dark" ? "custom-arrow":"custom-arrow2",
+                    }}
+                      title="Firebase"
+                      arrow
+                      open={activeTooltip === 'firebase'}
+                      onClose={() => setActiveTooltip(null)}
+                      onOpen={() => setActiveTooltip('firebase')}
+                    >
+                      <motion.div
+                      whileHover={{
+                        rotate: 180,  
+                        scale: 1.15,    
+                        transition: {
+                          type: "spring",
+                          stiffness: 300,
+                          damping: 20,  
+                        },
+                      }}
+                      initial={{ opacity: 0 }} 
+                      animate={{ opacity: 1 }}  
+                      exit={{ opacity: 0 }}   
+                        onClick={() => handleTooltipClick('firebase')}
+                        className="w-10 h-10 cursor-pointer donation rounded-full flex items-center justify-center"
+                      >
+                        <img className="h-8 w-8" src="/firebase.svg" alt="Firebase Logo" />
+                      </motion.div>
+                    </Tooltip>
+
+                    <Tooltip
+                     PopperProps={{
+                      modifiers: [
+                        {
+                          name: "offset",
+                          options: {
+                            offset: [0, -4]
+                          },
+                        },
+                      ],
+                    }}
+                    classes={{
+                      tooltip: mode === "dark" ? "custom-tooltip" : "custom-tooltip2",
+                      arrow:mode === "dark" ? "custom-arrow":"custom-arrow2",
+                    }}
+                      title="Vite"
+                      arrow
+                      open={activeTooltip === 'vite'}
+                      onClose={() => setActiveTooltip(null)}
+                      onOpen={() => setActiveTooltip('vite')}
+                    >
+                      <motion.div
+                      whileHover={{
+                        rotate: 180,  
+                        scale: 1.15,    
+                        transition: {
+                          type: "spring",
+                          stiffness: 300,
+                          damping: 20,  
+                        },
+                      }}
+                      initial={{ opacity: 0 }} 
+                      animate={{ opacity: 1 }}  
+                      exit={{ opacity: 0 }}   
+                        onClick={() => handleTooltipClick('vite')}
+                        className="w-10 h-10 cursor-pointer donation rounded-full flex items-center justify-center"
+                      >
+                        <img className="h-7 w-7" src="/vite.png" alt="Vite Logo" />
+                      </motion.div>
+                    </Tooltip>
+                 </div>
+               </div>
+
+               <div className='md:w-1/2 md:mb-0 mb-5  relative'>
+               <div className='md:absolute relative z-50  text-slate-950 text-3xl gap-5 text-center mb-2 w-full flex justify-center items-center' >
+               <motion.img
+  onClick={() => setShowOptions("iphone")}
+  src="/iphone.svg"
+  alt="Iphone"
+  className={`w-8 h-8 md:h-10 md:w-10 px-1 cursor-pointer rounded-xl ${mode === "dark" && showOptions === "iphone" ? "bg-gray-300" : "bg-gray-300"}`}
+  animate={{
+    scale: showOptions === "iphone" ? 1.15 : 1, // Enlarges when selected
+    boxShadow: showOptions === "iphone" ? "0px 4px 15px rgba(0, 0, 0, 0.2)" : "none",
+    backgroundColor: showOptions === "iphone" ? "rgba(255, 255, 255, 1)" : "rgba(0, 0, 0, 0)", 
+  }}
+  transition={{
+    duration: 0.2,
+    ease: "easeOut",
+  }}
+/>
+
+<motion.img
+  onClick={() => setShowOptions("laptop")}
+  src="/laptop2.svg"
+  alt="Laptop"
+  className={`w-8 h-8 md:h-10 md:w-10 p-1 cursor-pointer rounded-xl ${mode === "dark" && showOptions === "laptop" ? "bg-gray-300" : "bg-gray-300"}`}
+  animate={{
+    scale: showOptions === "laptop" ? 1.15 : 1,
+    boxShadow: showOptions === "laptop" ? "0px 4px 15px rgba(0, 0, 0, 0.2)" : "none",
+    backgroundColor: showOptions === "laptop" ? "rgba(255, 255, 255, 1)" : "rgba(0, 0, 0, 0)", // Transparent black
+  }}
+  transition={{
+    duration: 0.2,
+    ease: "easeOut",
+  }}
+/>
+
+               </div>
+               {showOptions === "laptop" && (
+   
+      <Gallery  images={kittyTaskMac} />
+    
+  )}
+  
+  {showOptions === "iphone" && (
+   
+      <Gallery images={kittyTaskPhone}  />
+   
+  )}
                </div>
             </div>
          </div>
